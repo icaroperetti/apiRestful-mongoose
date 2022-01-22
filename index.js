b484668968834e55a475bea8725ce59a578bc76c
Icaro Peretti <icaroperetti50@gmail.com>
2022-01-22 18:43:10 -0300

    Creating de server, mongo atlas connection and Person model

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+require("dotenv/config");
+
+const Person = require("./models/Person");
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World" });
+});
+
+const databaseurl = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wuenn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+mongoose
+  .connect(databaseurl)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server started on port 3000");
+    });
+  })
+  .catch((err) => console.log(err));
